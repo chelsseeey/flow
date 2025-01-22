899d8c5ffafdfd108ffd315417a1f2e42bb1e01c
chelsey <chelsey@ewha.ac.kr>
2025-01-22 17:46:32 +0900

    커스텀 환경 등록

--- flow/utils/registry.py
+++ flow/utils/registry.py
@@ -136,7 +136,10 @@ def env_constructor(params, version=0, render=None):
     """Return a constructor from make_create_env."""
     create_env, env_name = make_create_env(params, version, render)
     return create_env
-from flow.envs.multiagent.custom_traffic_light_figure_eight import TrafficLightFigureEightEnvregister(
+
+from flow.envs.multiagent.custom_traffic_light_figure_eight import TrafficLightFigureEightEnv
+
+register(
     id='traffic_light_figure_eight-v0',  # 버전 번호 포함
     entry_point='flow.envs.multiagent.custom_traffic_light_figure_eight:TrafficLightFigureEightEnv',
 )
