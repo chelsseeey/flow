2dcf486b5b0a3c0b333dfb6b72c021287f2b8f30
chelsey <chelsey@ewha.ac.kr>
2025-01-22 16:23:39 +0900

    PO_TrafficLightGridEnv

--- examples/exp_configs/non_rl/exp1.py
+++ examples/exp_configs/non_rl/exp1.py
@@ -9,7 +9,7 @@ from flow.core.params import SumoParams, EnvParams, NetParams
 from flow.core.params import VehicleParams, SumoCarFollowingParams
 from flow.envs.ring.accel import ADDITIONAL_ENV_PARAMS
 from flow.networks.figure_eight import ADDITIONAL_NET_PARAMS
-from flow.envs import PO_TrafficLightGridEnv
+from flow.envs.traffic_light_grid import TrafficLightGridPOEnv  # 최신 Env 클래스 임포트
 from flow.networks import FigureEightNetwork
 from flow.core.params import TrafficLightParams
 
@@ -69,7 +69,7 @@ flow_params = dict(
     exp_tag='figure8_with_lights',  # 실험 이름 변경
 
     # name of the flow environment the experiment is running on
-    env_name=PO_TrafficLightGridEnv,
+    env_name=TrafficLightGridPOEnv,
 
     # name of the network class the experiment is running on
     network=FigureEightNetwork,
