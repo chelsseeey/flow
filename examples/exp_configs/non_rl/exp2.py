cca09b3dd316af8e139b13e345da19e899b2cc0b
chaeyoung <chelsey@ewha.ac.kr>
2025-02-04 11:11:24 +0900

    exp2.py 수정

--- examples/exp_configs/non_rl/exp2.py
+++ examples/exp_configs/non_rl/exp2.py
@@ -1,38 +1,38 @@
-"""Example of a figure 8 network with human-driven vehicles.
-
-Right-of-way dynamics near the intersection causes vehicles to queue up on
-either side of the intersection, leading to a significant reduction in the
-average speed of vehicles in the network.
-"""
-from flow.controllers import IDMController, StaticLaneChanger, ContinuousRouter
-from flow.core.params import SumoParams, EnvParams, NetParams
+"""Figure eight example with traffic lights."""
+from flow.core.params import SumoParams, EnvParams, InitialConfig, NetParams, TrafficLightParams
 from flow.core.params import VehicleParams, SumoCarFollowingParams
-from flow.envs.ring.accel import ADDITIONAL_ENV_PARAMS
+from flow.controllers import IDMController, StaticLaneChanger, ContinuousRouter, RLController
 from flow.networks.figure_eight import ADDITIONAL_NET_PARAMS
-from flow.envs import AccelEnv
+from flow.envs.multiagent import MultiAgentAccelPOEnv  # 변경
 from flow.networks import FigureEightNetwork
-from flow.core.params import TrafficLightParams  # 신호등 설정 추가
-from flow.controllers.rlcontroller import RLController
 
+# time horizon of a single rollout
+HORIZON = 1500
+# number of rollouts per training iteration
+N_ROLLOUTS = 20
+# number of parallel workers
+N_CPUS = 2
 
+# Define the vehicles in the network.
+# Here we use one RL-controlled vehicle and 13 human-driven vehicles.
 vehicles = VehicleParams()
 
-# RL 차량 추가 (1대)
+# Add the RL-controlled vehicle (using the RLController and StaticLaneChanger)
 vehicles.add(
-    veh_id="rl",
-    acceleration_controller=(RLController, {}),  # RLController 추가
-    lane_change_controller=(StaticLaneChanger, {}),  # 필요 시 RL 컨트롤러로 변경
+    veh_id='rl',
+    acceleration_controller=(RLController, {}),
+    lane_change_controller=(StaticLaneChanger, {}),
     routing_controller=(ContinuousRouter, {}),
     car_following_params=SumoCarFollowingParams(
-        speed_mode=31,
+        speed_mode=31,  # using the original value (you can change this if needed)
         decel=2.5,
     ),
-    num_vehicles=1  # RL 차량 1대
+    num_vehicles=1
 )
 
-# 기존 IDM 차량 추가
+# Add idm vehicles
 vehicles.add(
-    veh_id="idm",
+    veh_id='idm',
     acceleration_controller=(IDMController, {}),
     lane_change_controller=(StaticLaneChanger, {}),
     routing_controller=(ContinuousRouter, {}),
@@ -44,62 +44,67 @@ vehicles.add(
     num_vehicles=13
 )
 
-# 신호등 설정 추가
+# Define traffic light settings.
 traffic_lights = TrafficLightParams(baseline=False)
 traffic_lights.add(
-    node_id="center",  # Figure Eight의 교차점 노드
-    tls_type="static",  # 고정 주기 신호등
-    programID="1",  # 프로그램 ID
+    node_id="center",   # Node at the intersection in the figure eight
+    tls_type="static",  # Static (fixed cycle) traffic light
+    programID="1",
     phases=[
-        {"duration": "5", "state": "GrGr"},  # 가로 초록
-        {"duration": "3", "state": "yrGr"},   # 가로 노랑
-        {"duration": "2", "state": "rrrr"},   # 모두 빨강
-        {"duration": "5", "state": "rGrG"},  # 세로 초록
-        {"duration": "3", "state": "ryrG"},   # 세로 노랑
-        {"duration": "2", "state": "rrrr"}    # 모두 빨강
+        {"duration": "5", "state": "GrGr"},  # Horizontal green
+        {"duration": "3", "state": "yrGr"},   # Horizontal yellow
+        {"duration": "2", "state": "rrrr"},   # All red
+        {"duration": "5", "state": "rGrG"},    # Vertical green
+        {"duration": "3", "state": "ryrG"},     # Vertical yellow
+        {"duration": "2", "state": "rrrr"}      # All red
     ]
 )
 
+# Define the main flow parameters dictionary.
 flow_params = dict(
-    # name of the experiment
-    exp_tag='figure8_with_lights',  # 실험 이름 변경
+    # Name of the experiment.
+    exp_tag='figure8_with_lights',
 
-    # name of the flow environment the experiment is running on
-    env_name=AccelEnv,
+    # The environment class to be used.
+    env_name=MultiAgentAccelPOEnv,
 
-    # name of the network class the experiment is running on
+    # The network class to be used.
     network=FigureEightNetwork,
 
-    # simulator that is used by the experiment
+    # The simulator to be used.
     simulator='traci',
 
-    # sumo-related parameters (see flow.core.params.SumoParams)
+    # SUMO-related parameters.
     sim=SumoParams(
-        render=True,
         sim_step=0.1,
+        render=True,
     ),
 
-    # environment related parameters (see flow.core.params.EnvParams)
+    # Environment parameters.
     env=EnvParams(
-        horizon=100000000,
-        additional_params=ADDITIONAL_ENV_PARAMS.copy(),
+        horizon=HORIZON,
+        additional_params={
+            'target_velocity': 20,
+            'max_accel': 3,
+            'max_decel': 3,
+            'sort_vehicles': False
+        },
     ),
 
-    # network-related parameters (see flow.core.params.NetParams and the
-    # network's documentation or ADDITIONAL_NET_PARAMS component)
+    # Network parameters.
     net=NetParams(
         additional_params=ADDITIONAL_NET_PARAMS.copy(),
     ),
 
-    # vehicles to be placed in the network at the start of a rollout (see
-    # flow.core.params.VehicleParams)
+    # Vehicles in the network.
     veh=vehicles,
 
-    # 신호등 설정 반영
+    # Initial configuration parameters.
+    initial=InitialConfig(),
+
+    # Include the traffic light settings.
     tls=traffic_lights
 )
 
-print(f"Traffic light parameters: {flow_params['tls']}")
-
-# flow_params 딕셔너리 출력
+print("Traffic light parameters:", flow_params['tls'])
 print("Flow parameters:", flow_params)
