b2b2efe3bb641a797d33f08b69dd767e6be47bdd
chaeyoung <chelsey@ewha.ac.kr>
2025-02-11 15:45:48 +0900

    .

--- examples/exp_configs/rl/singleagent/exp2.py
+++ examples/exp_configs/rl/singleagent/exp2.py
@@ -5,6 +5,7 @@ from flow.controllers import IDMController, StaticLaneChanger, ContinuousRouter,
 from flow.networks.figure_eight import ADDITIONAL_NET_PARAMS
 from flow.envs.multiagent import MultiAgentAccelPOEnv  # 변경
 from flow.networks import FigureEightNetwork
+from warning_logger import collision_logger  # Add import
 
 # time horizon of a single rollout
 HORIZON = 1500
@@ -101,6 +102,9 @@ flow_params = dict(
 
     # Initial configuration parameters.
     initial=InitialConfig(),
+
+    # Add collision logger
+    callback=collision_logger,  # Add logger to flow_params
 
     # Include the traffic light settings.
     tls=traffic_lights
