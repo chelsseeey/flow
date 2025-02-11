4ad45c11d8cff1e4547add9e227ce1c15f55e01f
chaeyoung <chelsey@ewha.ac.kr>
2025-02-11 14:35:03 +0900

    .

--- examples/warning_logger.py
+++ examples/warning_logger.py
@@ -1,31 +1,13 @@
 import io
 from contextlib import redirect_stdout
 import matplotlib.pyplot as plt
-import numpy as np
-from datetime import datetime
-import os
 
 class CollisionLogger:
     def __init__(self):
+        self.collision_count = 0
         self.iteration_collisions = []
-        self._setup_plotting()
-        self._setup_logging()
-        
-    def _setup_plotting(self):
-        """Initialize matplotlib plot"""
-        plt.ion()
-        self.fig, self.ax = plt.subplots(figsize=(8, 5))
-        self._update_plot()
-        
-    def _setup_logging(self):
-        """Setup logging directory and file"""
-        self.log_dir = "collision_logs"
-        os.makedirs(self.log_dir, exist_ok=True)
-        self.log_file = f"{self.log_dir}/collisions_{datetime.now():%Y%m%d_%H%M%S}.txt"
         
     def __call__(self, env, worker):
-        """Track collisions in simulation"""
-        collision_count = 0
         f = io.StringIO()
         with redirect_stdout(f):
             state = env.reset()
@@ -36,42 +18,29 @@ class CollisionLogger:
                 
         output = f.getvalue()
         if "Collision detected at time step" in output:
-            collision_count += 1
-            self._log_collision(collision_count)
-            
-        self.iteration_collisions.append(collision_count)
-        self._update_plot()
+            self.collision_count += 1
         
-    def _log_collision(self, count):
-        """Log collision to file"""
-        with open(self.log_file, 'a') as f:
-            f.write(f"{datetime.now():%Y-%m-%d %H:%M:%S} - Collision count: {count}\n")
-            
-    def _update_plot(self):
-        """Update matplotlib plot"""
-        if not hasattr(self, 'ax'):
-            self._setup_plotting()
-        self.ax.clear()
-        self.ax.set_xlabel('Iteration Number')
-        self.ax.set_ylabel('Number of Collisions')
-        self.ax.set_title('Collisions per Iteration')
-        self.ax.grid(True)
-        if self.iteration_collisions:
-            self.ax.plot(range(1, len(self.iteration_collisions) + 1), 
-                        self.iteration_collisions, 'g-')
-        plt.pause(0.01)
+        self.iteration_collisions.append(self.collision_count)
+        plot_collisions(self.iteration_collisions)
+        return self.collision_count
         
     def __getstate__(self):
-        """Make class serializable"""
-        state = self.__dict__.copy()
-        # Don't pickle matplotlib objects
-        del state['fig']
-        del state['ax']
-        return state
-    
+        return {
+            'collision_count': self.collision_count,
+            'iteration_collisions': self.iteration_collisions
+        }
+        
     def __setstate__(self, state):
-        """Restore from serialization"""
-        self.__dict__.update(state)
-        self._setup_plotting()
+        self.collision_count = state['collision_count']
+        self.iteration_collisions = state['iteration_collisions']
+
+def plot_collisions(collision_data):
+    plt.figure()
+    plt.plot(range(1, len(collision_data) + 1), collision_data, 'g-')
+    plt.xlabel('Iteration Number')
+    plt.ylabel('Number of Collisions')
+    plt.title('Collisions per Iteration')
+    plt.grid(True)
+    plt.pause(0.01)
 
 collision_logger = CollisionLogger()
